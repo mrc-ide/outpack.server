cd61b98d30104494e43efb01e336d4b5ace548bc
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-08-12 15:30:43 +0100

    Update cli

--- R/main.R
+++ R/main.R
@@ -1,6 +1,6 @@
 parse_main <- function(args = commandArgs(TRUE)) {
   usage <- "Usage:
-outpack.server [options]
+outpack.server [options] <path>
 
 Options:
   --log-level=LEVEL  Log-level (off, info, all) [default: info]
@@ -9,11 +9,12 @@ Options:
   dat <- docopt::docopt(usage, args)
   list(log_level = dat$log_level,
        validate = dat$validate,
-       port = as.integer(dat$port))
+       port = as.integer(dat$port),
+       path = dat$path)
 }
 
 
 main <- function(args = commandArgs(TRUE)) {
   dat <- parse_main(args)
-  api(dat$validate, dat$log_level)$run("0.0.0.0", port = dat$port)
+  api(dat$path, dat$validate, dat$log_level)$run("0.0.0.0", port = dat$port)
 }
